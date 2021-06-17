6dd288b00379208c5463193fe363903ec320956d
issam_emiketic <issam.bouhoush@emiketic.com>
2021-06-17 19:39:50 +0200

    add sound

--- src/Component/SnakeGame/index.js
+++ src/Component/SnakeGame/index.js
@@ -1,8 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react";
-import Snake from "./Snake/Snake";
-import Food from "./Food/Food";
+import React, { useCallback, useEffect, useState } from 'react';
+import Snake from './Snake/Snake';
+import Food from './Food/Food';
+import useSound from 'use-sound';
+import backgroundMusique from '../../sounds/preview.wav';
+import eatSuccess from '../../sounds/eat_success.wav';
+import game_over from '../../sounds/game_over.wav';
 
-import "./snake.css";
+import './snake.css';
 
 const getRandomCoordinates = () => {
   let min = 1;
@@ -13,17 +17,48 @@ const getRandomCoordinates = () => {
 };
 
 const SnakeGame = () => {
+  const [reset, setReset] = useState(true);
+  // MUSIC STATE
+  const [play] = useSound(backgroundMusique);
+  const [onEatSuccess] = useSound(eatSuccess);
+  const [gameOver] = useSound(game_over);
+
+  const [startMusic, setStartMusic] = useState(false);
+  const [gameOverEffect, setGameOverEffect] = useState(false);
+
   const [food, setFood] = useState(getRandomCoordinates);
   const [speed, setSpeed] = useState(200);
-  const [direction, setDirection] = useState("RIGHT");
+  const [direction, setDirection] = useState('RIGHT');
   const [snakeDots, setSnakeDots] = useState([
     [0, 0],
     [2, 0],
   ]);
   const [pause, setPause] = useState(true);
 
+  // console.log('direction', direction);
+  // useEffect(() => {
+  //   if (direction === null) {
+  //     setPause(true);
+  //     setSnakeDots([
+  //       [0, 0],
+  //       [2, 0],
+  //     ]);
+  //   }
+  // }, [direction]);
+
+  // MUSIC FCT
   useEffect(() => {
-    if (pause) return;
+    !!checkIfEat() && onEatSuccess();
+    const music = startMusic && play();
+    let lunchMusic = setInterval(music, 1000);
+    return () => clearInterval(lunchMusic);
+  });
+
+  useEffect(() => {
+    if (pause) {
+      setStartMusic(false);
+      return;
+    }
     checkIfOutOfBorders();
     checkIfCollapsed();
     setTimeout(() => moveSnake(snakeDots, checkIfEat()), speed);
@@ -35,17 +70,17 @@ const SnakeGame = () => {
       e = e || window.event;
       switch (e.keyCode) {
         case 38:
-          console.log("direction", direction);
-          !["DOWN", "UP"].includes(direction) && setDirection("UP");
+          console.log('direction', direction);
+          !['DOWN', 'UP'].includes(direction) && setDirection('UP');
           break;
         case 40:
-          !["DOWN", "UP"].includes(direction) && setDirection("DOWN");
+          !['DOWN', 'UP'].includes(direction) && setDirection('DOWN');
           break;
         case 37:
-          !["LEFT", "RIGHT"].includes(direction) && setDirection("LEFT");
+          !['LEFT', 'RIGHT'].includes(direction) && setDirection('LEFT');
           break;
         case 39:
-          !["LEFT", "RIGHT"].includes(direction) && setDirection("RIGHT");
+          !['LEFT', 'RIGHT'].includes(direction) && setDirection('RIGHT');
           break;
 
         default:
@@ -53,31 +88,31 @@ const SnakeGame = () => {
       }
     };
 
-    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener('keydown', onKeyDown);
 
     return () => {
-      document.removeEventListener("keydown", onKeyDown);
-      console.log("direction return", direction); // useEffect precedente
+      document.removeEventListener('keydown', onKeyDown);
+      console.log('direction return', direction); // useEffect precedente
     };
   }, [direction, setDirection]);
 
- 
   const moveSnake = useCallback(
     (snakeDots, eaten) => {
+      setStartMusic(true);
       let dots = [...snakeDots];
       let head = dots[dots.length - 1];
 
       switch (direction) {
-        case "RIGHT":
+        case 'RIGHT':
           head = [head[0] + 2, head[1]];
           break;
-        case "LEFT":
+        case 'LEFT':
           head = [head[0] - 2, head[1]];
           break;
-        case "DOWN":
+        case 'DOWN':
           head = [head[0], head[1] + 2];
           break;
-        case "UP":
+        case 'UP':
           head = [head[0], head[1] - 2];
           break;
 
@@ -92,7 +127,7 @@ const SnakeGame = () => {
         setSnakeDots([...dots]);
       }
     },
-    [direction]
+    [direction],
   );
 
   const checkIfOutOfBorders = () => {
@@ -115,26 +150,39 @@ const SnakeGame = () => {
 
   const checkIfEat = () => {
     let head = snakeDots[snakeDots.length - 1];
-
     return head[0] === food[0] && head[1] === food[1];
   };
 
   const onGameOver = () => {
+    handleGameOverEffect();
     setSnakeDots([
       [0, 0],
       [2, 0],
     ]);
     setDirection(null);
+    setReset(false);
   };
+
+  const handleGameOverEffect = () => {
+    setGameOverEffect(true);
+
+    setTimeout(() => {
+      setGameOverEffect(false);
+      // setPause(true);
+    }, 1000);
+    gameOver();
+  };
+
   return (
     <>
       <div className="game-area">
+        <div className={`${gameOverEffect && 'game-over'}`}></div>
         <Snake snakeDots={snakeDots} />
         <Food dot={food} />
       </div>
       <div className="btn">
         <button onClick={() => setPause((p) => !p)}>
-          {pause ? "Play" : "Pause"}
+          {pause ? 'Play' : 'Pause'}
         </button>
       </div>
     </>
